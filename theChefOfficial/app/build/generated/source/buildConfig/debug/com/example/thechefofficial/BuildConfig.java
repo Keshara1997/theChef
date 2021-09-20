a21fffc3341c4b4e690cc43ea96c876bac55bcba
Kavinda Keshara <63952119+keshara1997@users.noreply.github.com>
2021-09-20 19:10:32 +0530

    Initial commit

---
+++ theChefOfficial/app/build/generated/source/buildConfig/debug/com/example/thechefofficial/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.thechefofficial;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.thechefofficial";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
